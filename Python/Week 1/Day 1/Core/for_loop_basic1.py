7d0839c7237f863add622fe739d5c9f83b4aa01c
RayenTB <rayentborski2@gmail.com>
2023-11-16 16:36:29 +0100

    Python+

---
+++ Python/Week 1/Day 1/Core/for_loop_basic1.py
@@ -0,0 +1,27 @@
+for i in range(0,151):# or (151)
+    print(i)
+
+for i in range(5, 1001, 5):
+    print(i)
+    
+    for i in range(1, 101):
+        if i % 10 == 0:
+         print("Coding Dojo")
+        elif i % 5 == 0:
+         print("Coding")
+    else:
+        print(i)
+
+for i in range(0, 500):
+ if i % 2 == 0:
+    print(i)#final sum   
+        
+for i in range(2018,0,-4):
+   print(i) 
+
+highnum = 9
+lownum = 2
+mult = 3
+for i in range(lownum,highnum+1,): #(lownum,highnum+1,3)
+    if i % mult == 0:
+        print(i)

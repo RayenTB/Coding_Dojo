b463ae103dad922481ae7b0b8ba9c18b21a46717
RayenTB <rayentborski2@gmail.com>
2023-11-30 15:49:11 +0100

    Updated

--- Python/Week 1/Day 2/Core/Bankacc.py
+++ Python/Week 1/Day 2/Core/Bankacc.py
@@ -1,10 +1,23 @@
 class BankAccount:
-    def __init__(self, balance=100, interest_rate=50):
+    num_accounts = 0  
+
+    @classmethod
+    def print_all_accounts_info(cls):
+        for acc in cls.all_accounts:
+            print(f"Account {acc.account_number}: Balance - ${acc.balance}, Interest Rate - {acc.interest_rate}")
+
+    all_accounts = []
+
+    def __init__(self, int_rate=0.01, balance=0):
+        self.interest_rate = int_rate
         self.balance = balance
-        self.interest_rate = interest_rate
+        self.account_number = BankAccount.num_accounts + 1
+        BankAccount.num_accounts += 1
+        BankAccount.all_accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
+        return self
 
     def withdraw(self, amount):
         if self.balance >= amount:
@@ -12,11 +25,20 @@ class BankAccount:
         else:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
+        return self
 
-    def display_account_info(self): 
+    def display_account_info(self):
         print(f"Balance: ${self.balance}")
+        return self
 
     def yield_interest(self):
         if self.balance > 0:
-            interest = self.balance * self.interest_rate
-            self.balance += interest
+            self.balance += self.balance * self.interest_rate
+        return self
+
+account1 = BankAccount(0.02, 1000)
+account2 = BankAccount(0.03, 500)
+account1.deposit(100).deposit(200).deposit(300).withdraw(150).yield_interest().display_account_info()
+account2.deposit(50).deposit(100).withdraw(20).withdraw(30).withdraw(150).withdraw(50).yield_interest().display_account_info()
+BankAccount.print_all_accounts_info()
+

3f96d9e1fb7b067ab0499a2050b1d5ea53ab52ae
RayenTB <rayentborski2@gmail.com>
2023-12-04 09:14:08 +0100

    Updated

---
+++ Python/Week 2/Day 2/Core/Dojos and Ninjas/dojos.sql
@@ -0,0 +1,5 @@
+SELECT * FROM ninjas;
+INSERT INTO ninjas (firt_name,last_name,age,dojo_id)
+VALUES ("walid","benmousa",25,4),("khaled","hannechi",30,4),("lazher","arouch",27,4),
+("azizos","benmousa",25,5),("khaled","hannechi",30,5),("lazher","arouch",27,5),
+("walid","benmousa",25,6),("khaled","hannechi",30,6),("lazher","arouch",27,6);
